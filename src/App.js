c59fa0919a3ae3f1b125f888bd22f0e9a859ff8c
Nada-Yakout-Shahein <nadayakout171@gmail.com>
2023-10-14 16:23:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="nnn">hi nada</h1>
+      <h2 className="nnn">bie nada</h2>
+    </>
+  );
+}
+
+export default App;

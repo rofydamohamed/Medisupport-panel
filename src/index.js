ad29c2b6cf13e6f36529080de7cebda65fc37931
Nada-Yakout-Shahien <nadayakout171@gmail.com>
2024-03-25 23:02:38 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,35 +1,95 @@
 import React from "react";
+import { AuthProvider } from "./components/AuthContext";
+import ProtectedRoute from './components/ProtectedRoute';
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/home";
-import Html from "./pages/html";
-import Css from "./pages/css";
-import Javascript from "./pages/javascript";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Loginasadmin from "./pages/Login_as_admin";
+import Loginasdoctor from "./pages/Login_as_doctor";
+import Contactus from "./pages/Contact_us";
+import Users from "./pages/Users";
+import Articles from "./pages/Articles";
+import Doctors from "./pages/Doctors";
+import RegisterNewDoctor from "./pages/Register_New_Doctor";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-    errorElement: <h1>SORRY...........</h1>,
+    path: "/Dashboard",
+    element: <Dashboard />,
+    errorElement: <h1>Sorry, No page to display....</h1>,
   },
   {
-    path: "/html",
-    element: <Html />,
+    path: "/Login",
+    element: (
+      <>
+        <Login />
+      </>
+    ),
+    errorElement: <h1>Sorry, No page to display....</h1>,
   },
   {
-    path: "/css",
-    element: <Css />,
+    path: "/Login_as_admin",
+    element: (
+      <>
+        <Loginasadmin />
+      </>
+    ),
+    errorElement: <h1>Sorry, No page to display....</h1>,
   },
   {
-    path: "/javascript",
-    element: <Javascript />,
+    path: "/Login_as_doctor",
+    element: <Loginasdoctor />,
+    errorElement: <h1>Sorry, No page to display....</h1>,
+  },
+  {
+    path: "/Contact_us",
+    element: <Contactus/>,
+    errorElement: <h1>Sorry, No page to display....</h1>,
+  },
+  {
+    path: "/Users",
+    element: <Users />,
+    errorElement: <h1>Sorry, No page to display....</h1>,
+  },
+  {
+    path: "/Articles",
+    element: (
+      <>
+        <Articles />
+      </>
+    ),
+    errorElement: <h1>Sorry, No page to display....</h1>,
+  },
+  {
+    path: "/Doctors",
+    element: (
+      <ProtectedRoute>
+        <Doctors />
+      </ProtectedRoute>
+    ),
+    errorElement: <h1>Sorry, No page to display....</h1>,
+  },
+  {
+    path: "/Register_New_Doctor",
+    element: (
+      <ProtectedRoute>
+        <RegisterNewDoctor />
+      </ProtectedRoute>
+    ),
+    errorElement: <h1>Sorry, No page to display....</h1>,
   },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );

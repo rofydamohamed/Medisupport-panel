85e3f46331f93671f7a88c9f4e04159a7f555c16
Nada-Yakout-Shahien <nadayakout171@gmail.com>
2024-06-15 14:44:15 +0300

    add

--- src/pages/Register_New_Doctor.js
+++ src/pages/Register_New_Doctor.js
@@ -2,16 +2,13 @@ import React, { useState, useRef } from "react";
 import { Helmet } from "react-helmet-async";
 import Dashboard from "../components/Dashboard";
 import "./Register_New_Doctor.css";
+import { RegisterNewDoctor } from "../components/apiService";
 import { NavLink } from "react-router-dom";
-import {
-  RegisterNewDoctor,
-  saveTokenToLocalStorage,
-} from "../components/apiService";
 
 const Register_New_Doctor = () => {
   const [selectedFile, setSelectedFile] = useState(null);
-  const [accessToken, setAccessToken] = useState("");
   const hiddenFileInputRef = useRef(null);
+  const [isOverlayVisible, setIsOverlayVisible] = useState(false);
 
   const handleClick = (event) => {
     event.stopPropagation();
@@ -22,7 +19,7 @@ const Register_New_Doctor = () => {
     const file = event.target.files[0];
     setSelectedFile(file);
   };
-
+  //after-click-btn
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -52,13 +49,16 @@ const Register_New_Doctor = () => {
       }
       const response = await RegisterNewDoctor(doctorData, accessToken);
       console.log("Doctor registered successfully:", response);
+      setIsOverlayVisible(true);
       event.target.reset();
       setSelectedFile(null);
     } catch (error) {
       console.error("Error registering doctor:", error);
     }
   };
-
+  const handleHideOverlay = () => {
+    setIsOverlayVisible(false);
+  };
   return (
     <>
       <Helmet>
@@ -296,6 +296,44 @@ const Register_New_Doctor = () => {
               Register
             </button>
           </form>
+          {isOverlayVisible && (
+            <div className="overlay">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="300"
+                height="300"
+                viewBox="0 0 300 300"
+                fill="none"
+              >
+                <path
+                  d="M150 281.25C115.19 281.25 81.8064 267.422 57.1922 242.808C32.5781 218.194 18.75 184.81 18.75 150C18.75 115.19 32.5781 81.8064 57.1922 57.1922C81.8064 32.5781 115.19 18.75 150 18.75C184.81 18.75 218.194 32.5781 242.808 57.1922C267.422 81.8064 281.25 115.19 281.25 150C281.25 184.81 267.422 218.194 242.808 242.808C218.194 267.422 184.81 281.25 150 281.25ZM150 300C189.782 300 227.936 284.196 256.066 256.066C284.196 227.936 300 189.782 300 150C300 110.218 284.196 72.0644 256.066 43.934C227.936 15.8035 189.782 0 150 0C110.218 0 72.0644 15.8035 43.934 43.934C15.8035 72.0644 0 110.218 0 150C0 189.782 15.8035 227.936 43.934 256.066C72.0644 284.196 110.218 300 150 300Z"
+                  fill="#4A963D"
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="73.825"
+                  y="89"
+                  width="153"
+                  height="122"
+                  viewBox="0 0 153 122"
+                  fill="none"
+                >
+                  <path
+                    d="M128.687 4.18738C128.554 4.31683 128.429 4.45461 128.312 4.59989L63.1937 87.5686L23.95 48.3061C21.2842 45.8221 17.7583 44.4698 14.1152 44.5341C10.4721 44.5984 6.99608 46.0742 4.41959 48.6507C1.8431 51.2272 0.367251 54.7032 0.302972 58.3463C0.238693 61.9895 1.591 65.5154 4.075 68.1812L53.6875 117.812C55.024 119.146 56.6156 120.198 58.3672 120.903C60.1188 121.609 61.9946 121.955 63.8827 121.92C65.7708 121.885 67.6325 121.47 69.3568 120.7C71.0811 119.93 72.6326 118.82 73.9187 117.437L148.769 23.8749C151.317 21.1998 152.711 17.6292 152.648 13.9349C152.585 10.2406 151.071 6.71954 148.432 4.13291C145.794 1.54627 142.244 0.10192 138.549 0.112114C134.854 0.122308 131.312 1.58623 128.687 4.18738Z"
+                    fill="#4A963D"
+                  />
+                </svg>
+              </svg>{" "}
+              <NavLink to="/Dash" className="done">
+                Done{" "}
+              </NavLink>
+              <button onClick={handleHideOverlay}>
+                <NavLink className="Registernew" to="/Register">
+                  Register Anthor
+                </NavLink>
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </>
